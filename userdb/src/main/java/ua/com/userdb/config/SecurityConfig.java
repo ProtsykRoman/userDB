ffdc5f941abbeb457904493513921c2b0b21fea4
Admin <Admin@DESKTOP-0QAGGIH>
2026-02-23 14:02:26 +0200

    change report and list pages

--- userdb/src/main/java/ua/com/userdb/config/SecurityConfig.java
+++ userdb/src/main/java/ua/com/userdb/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
                 .permitAll()
-            )
-            .csrf(csrf -> csrf.disable());
+            );
 
         return http.build();
     }
